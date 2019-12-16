929315fa1991e77b82b44ccf616193f4b6c872aa
pzuradzki <paulzuradzki@gmail.com>
2019-12-16 15:27:48 -0600

    cleanup

--- sockets/threading_demo/countdown.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def countdown(n):
-    while n > 0:
-        print('T-minus', ne)
-        time.sleep(5)
-        n -= 1
-
-
--- sockets/zmq_client.py
+++
@@ -1,9 +0,0 @@
-import zmq
-ocntext = zmq.Context()
-sock = context.socket(zmq.REP) # reply socket
-sock.bind('tcp://*:30000')
-msg = sock.recv()
-
-
-# >>> msg
-# >>> sock.send(b'Hey there')
